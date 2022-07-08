a6a61a42c46c9053d440cfe60e19e6f9de798fe6
pistachi73 <oscarpulido98@gmail.com>
2022-07-08 18:43:56 +0200

    adding features

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import styled from "styled-components";
 export interface ButtonProps {
   label: string;
 }
 
+const StyledButton = styled.button`
+  padding: 30px;
+  background-color: red;
+`;
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <StyledButton>{props.label}</StyledButton>;
 };
 
 export default Button;
