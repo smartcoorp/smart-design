4181d715b47afbcb211544ef6c3208b66582f30c
pistachi73 <oscarpulido98@gmail.com>
2022-07-13 17:16:53 +0200

    [SC-14] Add base styles (skip ci)

---
+++ src/global-styles/base-styles.tsx
@@ -0,0 +1,49 @@
+import { scale080, scale100, gray900 } from "./../tokens";
+import { createGlobalStyle } from "styled-components";
+
+export const BaseStyles = createGlobalStyle`
+  
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+  
+  body {
+    background: white;
+    color: black;
+    font-size: ${scale080};
+    line-height: ${scale100};
+    min-width: 320px;
+    margin: 0;
+  }
+  
+  body {
+    visibility: visible;
+  }
+  
+  main {
+    width: 100%;
+  }
+  
+  a {
+    text-decoration: none;
+  }
+  
+  a:hover,
+  a:focus {
+  }
+  p,
+  li {
+    font-size: ${scale080};
+    line-height: ${scale100};
+  }
+  
+  /* due to https://github.com/necolas/normalize.css/blob/master/CHANGELOG.md#500-october-3-2016 */
+  button,
+  input,
+  select,
+  textarea {
+    font-family: inherit;
+    line-height: inherit;
+  }`;
