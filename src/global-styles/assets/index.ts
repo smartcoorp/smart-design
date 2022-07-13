5c1f556f33d9a7d32e0de9a9baae503b546584a2
pistachi73 <oscarpulido98@gmail.com>
2022-07-13 18:13:35 +0200

    [SC-16] Add global fonts (skip ci)

---
+++ src/global-styles/assets/index.ts
@@ -0,0 +1,23 @@
+export const fontMontserratBoldTtf = require("./Montserrat-Bold.ttf");
+export const fontMontserratBoldWoff = require("./Montserrat-Bold.woff");
+export const fontMontserratBoldWoff2 = require("./Montserrat-Bold.woff2");
+
+export const fontMontserratBoldItalicTtf = require("./Montserrat-BoldItalic.ttf");
+export const fontMontserratBoldItalicWoff = require("./Montserrat-BoldItalic.woff");
+export const fontMontserratBoldItalicWoff2 = require("./Montserrat-BoldItalic.woff2");
+
+export const fontMontserratRegularTtf = require("./Montserrat-Regular.ttf");
+export const fontMontserratRegularWoff = require("./Montserrat-Regular.woff");
+export const fontMontserratRegularWoff2 = require("./Montserrat-Regular.woff2");
+
+export const fontMontserratItalicTtf = require("./Montserrat-Italic.ttf");
+export const fontMontserratItalicWoff = require("./Montserrat-Italic.woff");
+export const fontMontserratItalicWoff2 = require("./Montserrat-Italic.woff2");
+
+export const fontOswaldRegularTtf = require("./Oswald-Regular.ttf");
+export const fontOswaldRegularWoff = require("./Oswald-Regular.woff");
+export const fontOswaldRegularWoff2 = require("./Oswald-Regular.woff2");
+
+export const fontOswaldSemiBoldTtf = require("./Oswald-SemiBold.ttf");
+export const fontOswaldSemiBoldWoff = require("./Oswald-SemiBold.woff");
+export const fontOswaldSemiBoldWoff2 = require("./Oswald-SemiBold.woff2");
---
+++ src/global-styles/font-styles.tsx
@@ -0,0 +1,75 @@
+import { createGlobalStyle } from "styled-components";
+import * as F from "./assets";
+
+const fonts = [
+  {
+    fontFamily: "Montserrat",
+    fontWeight: 400,
+    fontStyle: "normal",
+    woff2: F.fontMontserratRegularWoff2,
+    woff: F.fontMontserratRegularWoff,
+    ttf: F.fontMontserratRegularTtf,
+  },
+  {
+    fontFamily: "Montserrat",
+    fontWeight: 400,
+    fontStyle: "italic",
+    woff2: F.fontMontserratItalicWoff2,
+    woff: F.fontMontserratItalicWoff,
+    ttf: F.fontMontserratItalicTtf,
+  },
+  {
+    fontFamily: "Montserrat",
+    fontWeight: 700,
+    fontStyle: "bold",
+    woff2: F.fontMontserratBoldWoff2,
+    woff: F.fontMontserratBoldWoff,
+    ttf: F.fontMontserratBoldTtf,
+  },
+  {
+    fontFamily: "Montserrat",
+    fontWeight: 700,
+    fontStyle: "bold",
+    woff2: F.fontMontserratBoldItalicWoff2,
+    woff: F.fontMontserratBoldItalicWoff,
+    ttf: F.fontMontserratBoldItalicTtf,
+  },
+
+  {
+    fontFamily: "Oswald",
+    fontWeight: 400,
+    fontStyle: "normal",
+    woff2: F.fontOswaldRegularWoff2,
+    woff: F.fontOswaldRegularWoff,
+    ttf: F.fontOswaldRegularTtf,
+  },
+  {
+    fontFamily: "Oswald",
+    fontWeight: 600,
+    fontStyle: "normal",
+    woff2: F.fontOswaldSemiBoldWoff2,
+    woff: F.fontOswaldSemiBoldWoff,
+    ttf: F.fontOswaldSemiBoldTtf,
+  },
+];
+
+const fontsString = fonts
+  .map(({ fontFamily, fontWeight, fontStyle, woff, woff2, ttf }) => {
+    return `
+      @font-face {
+        font-family: ${fontFamily};
+        font-display: block;
+        font-style: ${fontStyle};
+        font-weight: ${fontWeight};
+        src: url(${woff2}) format("woff2"),
+        url(${woff}) format("woff"),
+        url(${ttf}) format('truetype');
+
+      }`;
+  })
+  .join("")
+  .replace(/\n|^\s+/gm, "");
+
+export const FontStyles = createGlobalStyle`
+    ${fontsString}
+`;
