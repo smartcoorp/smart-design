4d63d314544b5f508dc364d3e18d181876412bb1
pistachi73 <oscarpulido98@gmail.com>
2022-07-27 16:55:59 +0200

    add ring style

---
+++ src/styles/ring.ts
@@ -0,0 +1,12 @@
+import { css } from "styled-components";
+import { scale010, spaceXXS } from "../tokens";
+
+type RingProps = {
+  width?: string;
+  color: string;
+  offsetColor?: string;
+  offset?: string;
+};
+export const ring = ({ width = scale010, color, offsetColor, offset = spaceXXS }: RingProps) => css`
+  box-shadow: 0 0 0 ${offset} ${offsetColor ?? "white"}, 0 0 0 calc(${width} + ${offset}) ${color};
+`;
