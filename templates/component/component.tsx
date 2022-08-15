ed342a9cb7e1bf8401ea625fb4c033f4060b94c0
pistachi73 <oscarpulido98@gmail.com>
2022-08-15 18:55:33 +0200

    add className and children to template

--- templates/component/component.tsx
+++ templates/component/component.tsx
@@ -3,7 +3,7 @@ import { Styled } from "./component.styles";
 import { ComponentProps } from "./component.types";
 
 export const Component: React.FC<ComponentProps> = ({ children, className }) => {
-  return <Styled.Container>Container</Styled.Container>;
+  return <Styled.Container className={className}>{children}</Styled.Container>;
 };
 
 Component.displayName = "Component";
