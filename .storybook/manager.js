332fdd2d4e5f2ba6ac64968fb8b265da61cb4642
pistachi73 <oscarpulido98@gmail.com>
2022-09-04 15:45:21 +0200

    add custom stroybook theme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,8 @@
 // in .storybook/manager.js
 import addons from "@storybook/addons";
+import smartCookieTheme from "./smartcookieTheme";
 
 addons.setConfig({
   enableShortcuts: false,
+  theme: smartCookieTheme,
 });
---
+++ .storybook/smartcookieTheme.js
@@ -0,0 +1,11 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  appBorderRadius: 8,
+
+  brandTitle: "SmartCookie Storybook",
+  brandUrl: "https://cookiecoaching.com",
+  brandImage: "https://storage.cloud.google.com/smartcookie_logos/SC_Logo.png",
+  brandTarget: "_self",
+});
